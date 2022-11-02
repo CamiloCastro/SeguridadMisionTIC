4084ca2588bf24bbc8748522c6aa42f40bd23b80
Juan Camilo Castro Pinto <jccastrop@unal.edu.co>
2022-11-01 22:07:23 -0500

    Adición del modelo y repositorios CRUD

--- src/main/java/tutorial/misiontic/seguridad/model/PermisoRol.java
+++ src/main/java/tutorial/misiontic/seguridad/model/PermisoRol.java
@@ -1,7 +1,14 @@
 package tutorial.misiontic.seguridad.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
 public class PermisoRol {
 
+    @Id
     private String _id;
 
 }
--- src/main/java/tutorial/misiontic/seguridad/model/Usuario.java
+++ src/main/java/tutorial/misiontic/seguridad/model/Usuario.java
@@ -1,10 +1,24 @@
 package tutorial.misiontic.seguridad.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
 public class Usuario {
 
+    @Id
     private String _id;
     private String seudonimo;
     private String correo;
     private String contrasena;
+
+    public Usuario(String seudonimo, String correo, String contrasena)
+    {
+        this.seudonimo = seudonimo;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
 
 }
